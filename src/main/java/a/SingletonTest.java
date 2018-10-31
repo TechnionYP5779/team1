081482efa5042d93d239307c9abae7eb879ccdf6
yoavz <yoavzuriel@cs.technion.ac.il>
2018-10-31 13:37:09 +0200

    Tested singleton, issue #27

---
+++ src/main/java/a/SingletonTest.java
@@ -0,0 +1,21 @@
+package a;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+
+public class SingletonTest {
+    @Test
+    public void testToList() {
+        assertEquals(Arrays.asList("hello"), singleton.list("hello"));
+    }
+
+    @Test
+    public void testToArray() {
+        Object[] a = singleton.array("hello");
+        assertEquals("hello", a[0]);
+        assertEquals(1, a.length);
+    }
+}
