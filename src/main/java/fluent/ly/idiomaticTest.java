2228bd84c4ca99652b3d802d67ed51066ac6caa1
shai <shai.zeevi@campus.technion.ac.il>
2018-11-05 20:48:36 +0200

    moved TEST class to idiomaticTest. reaching 86% coverage in tests of class idiomatic. #16 #29

--- src/main/java/fluent/ly/idiomaticTest.java
+++ src/main/java/fluent/ly/idiomaticTest.java
@@ -2,6 +2,8 @@ package fluent.ly;
 
 import static org.junit.Assert.*;
 
+import static fluent.ly.azzert.*;
+
 import java.util.function.*;
 
 import org.junit.*;
@@ -46,20 +48,28 @@ public class idiomaticTest {
     assertEquals("'abc'", idiomatic.quote("abc"));
   }
 
-  @SuppressWarnings("static-method") @Test public void testRunDoesNotThrow() {
+  @SuppressWarnings("static-method") @Test public void testRunDoesNotThrow_SanityCheck() {
     Runnable r = () -> {@SuppressWarnings("unused") int x = 1;};
     idiomatic.Runner runner = idiomatic.run(r);
     try{
-      runner.run();    
+      runner.run();
+      runner.when(false);
+      runner.when(true);
+      runner.unless(false);
+      runner.unless(true);
     } catch(Exception e) {
       e.printStackTrace();
       fail("Should not throw");
     }
   }
 
-  @SuppressWarnings("static-method") @Test public void testTake() {
+  @SuppressWarnings({ "static-method", "null" }) @Test public void testTake() {
     Storer<Boolean> sto = idiomatic.take(Boolean.valueOf(true));
     assertTrue(sto.get().booleanValue());
+    assertNull(sto.when(false));
+    assertTrue(sto.when(true).booleanValue());
+    assertNull(sto.unless(true));
+    assertTrue(sto.unless(false).booleanValue());
   }
 
   @SuppressWarnings("static-method") @Test public void testUnlessBoolean() {
@@ -75,5 +85,57 @@ public class idiomaticTest {
   @SuppressWarnings("static-method") @Test public void testWhen() {
     assertNull(idiomatic.when(false).eval(Integer.valueOf(1)));
     assertEquals(Integer.valueOf(1), idiomatic.when(true).eval(Integer.valueOf(1)));
+  }
+  
+  @Test public void use0() {
+    assert new Storer<>(this) != null;
+  }
+
+  @SuppressWarnings("static-method") @Test public void use08() {
+    azzert.isNull(idiomatic.unless(true).eval(() -> new Object()));
+  }
+
+  @SuppressWarnings("static-method") @Test public void use09() {
+    assert idiomatic.unless(false).eval(() -> new Object()) != null;
+  }
+
+  @Test public void use1() {
+    assert new Storer<>(this) != null;
+    new Storer<>(this).when(true);
+  }
+
+  @SuppressWarnings("static-method") @Test public void use10() {
+    assert idiomatic.when(true).eval(() -> new Object()) != null;
+  }
+
+  @SuppressWarnings("static-method") @Test public void use11() {
+    azzert.isNull(idiomatic.when(false).eval(() -> new Object()));
+  }
+
+  @Test public void use2() {
+    assert idiomatic.take(this) != null;
+    azzert.isNull(idiomatic.take(this).when(false));
+  }
+
+  @Test public void use3() {
+    azzert.that(idiomatic.take(this).when(true), is(this));
+  }
+
+  @Test public void use4() {
+    azzert.isNull(idiomatic.take(this).when(false));
+  }
+
+  @Test public void use5() {
+    azzert.that(idiomatic.take(this).unless(false), is(this));
+  }
+
+  @Test public void use6() {
+    azzert.isNull(idiomatic.take(this).unless(true));
+  }
+
+  @Test public void use7() {
+    azzert.isNull(idiomatic.take(this).unless(true));
+    azzert.isNull(idiomatic.take(null).unless(true));
+    azzert.isNull(idiomatic.take(null).unless(false));
   }
 }
