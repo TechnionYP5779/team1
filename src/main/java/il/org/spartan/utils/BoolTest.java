8514b6f202633c85f5743e5d00b1e4706904b666
shai <shai.zeevi@campus.technion.ac.il>
2018-11-07 15:02:06 +0200

    boolTest refactored to use azzert #43

--- src/main/java/il/org/spartan/utils/BoolTest.java
+++ src/main/java/il/org/spartan/utils/BoolTest.java
@@ -4,34 +4,37 @@ import static org.junit.Assert.*;
 
 import org.junit.*;
 
+import fluent.ly.*;
+
 public class BoolTest {
   @Test @SuppressWarnings("static-method") public void testValueOf() {
-    assert Bool.valueOf(true).get();
-    assert !Bool.valueOf(false).get();
+    azzert.assertTrue(Bool.valueOf(true).get());
+    azzert.assertFalse(Bool.valueOf(false).get());
+    
   }
 
   @Test @SuppressWarnings("static-method") public void testClear() {
-    assert !Bool.valueOf(true).clear().get();
+    azzert.assertFalse(Bool.valueOf(true).clear().get());
   }
 
   @Test @SuppressWarnings("static-method") public void testGet() {
-    assert new Bool(true).get();
-    assert !new Bool(false).get();
-    assert !new Bool().get();
+    azzert.assertTrue(new Bool(true).get());
+    azzert.assertFalse(new Bool(false).get());
+    azzert.assertFalse(new Bool().get());
   }
 
   @Test @SuppressWarnings("static-method") public void testSet() {
-    assert Bool.valueOf(false).set().get();
+    azzert.assertTrue(Bool.valueOf(false).set().get());
   }
 
   @Test @SuppressWarnings("static-method") public void testSetBoolean() {
-    assert Bool.valueOf(false).set(true).get();
-    assert !Bool.valueOf(false).set(false).get();
+    azzert.assertTrue(Bool.valueOf(false).set(true).get());
+    azzert.assertFalse(Bool.valueOf(false).set(false).get());
   }
   
-  @Test @SuppressWarnings("static-method") public void testInner() {
-    assertEquals(new Bool(true).inner(), Boolean.TRUE);
-    assertEquals(new Bool(false).inner(), Boolean.FALSE);
-    assertEquals(new Bool().inner(), Boolean.FALSE);
+  @Test @SuppressWarnings({ "static-method", "static-access" }) public void testInner() {
+    azzert.assertEquals(new Bool(true).inner(), Boolean.TRUE);
+    azzert.assertEquals(new Bool(false).inner(), Boolean.FALSE);
+    azzert.assertEquals(new Bool().inner(), Boolean.FALSE);
   }
 }
