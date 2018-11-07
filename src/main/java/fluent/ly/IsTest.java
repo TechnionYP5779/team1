2bbfd81a04919e851f0572cb4f0bd38cbdb5d5bb
yoavz <yoavzuriel@cs.technion.ac.il>
2018-11-07 09:09:58 +0200

    added more tests to isTest #25

--- src/main/java/fluent/ly/IsTest.java
+++ src/main/java/fluent/ly/IsTest.java
@@ -2,16 +2,26 @@ package fluent.ly;
 
 import static org.junit.Assert.*;
 
-import java.util.*;
-
 import org.junit.*;
 
-public class IsTest {
-  @SuppressWarnings("static-method") @Test public void testIsNotIn() {
+@SuppressWarnings({ "static-method", "null" }) public class IsTest {
+  @Test public void testIsNotInAndOut() {
     assertFalse(is.in(Integer.valueOf(1), Integer.valueOf(8), Integer.valueOf(100), Integer.valueOf(2)));
+    assertTrue(is.out(Integer.valueOf(1), Integer.valueOf(8), Integer.valueOf(100), Integer.valueOf(2)));
+  }
+
+  @Test public void testIsInNotOut() {
+    assertTrue(is.in(Integer.valueOf(8), Integer.valueOf(8), Integer.valueOf(100), Integer.valueOf(2)));
+    assertFalse(is.out(Integer.valueOf(8), Integer.valueOf(8), Integer.valueOf(100), Integer.valueOf(2)));
+  }
+
+  @Test public void testIsIntInNotIn() {
+    assertFalse(is.intIsIn(1, 2, 3, 4, 5, 6, 7));
+  }
+
+  @Test public void testIsIntIn() {
+    assertTrue(is.intIsIn(5, 5, 6, 7, 8, 9));
   }
   
-  public void testIsIn() {
-    assertFalse(is.in(Integer.valueOf(8), Integer.valueOf(8), Integer.valueOf(100), Integer.valueOf(2)));
-  }
+  
 }
