57e3a270f94122b30f42b55d2d9e7b1ecf9f1a10
saifun <saifunny@gmail.com>
2018-11-10 15:08:53 +0200

    Issue #47 - Handling the undefined behaviour of linear regression for one dot

--- src/main/java/il/org/spartan/utils/LinearRegression.java
+++ src/main/java/il/org/spartan/utils/LinearRegression.java
@@ -4,6 +4,11 @@ package il.org.spartan.utils;
   double[] xVals;
   double[] yVals;
   
+  public class NotEnoughDots extends Exception {
+    private static final long serialVersionUID = 1L;
+    // End of list exception
+  }
+  
   LinearRegression(RealNumbersPairList list){
     xVals = new double[list.size()];
     yVals = new double[list.size()];
@@ -15,8 +20,8 @@ package il.org.spartan.utils;
     }
   }
   
-  public LinearFunction getLine() {
-    
+  public LinearFunction getLine() throws NotEnoughDots {
+    if(xVals.length <= 1) throw new NotEnoughDots();
     if(xVals.length == 2) {
       double slope = (yVals[1] - yVals[0])/(xVals[1] - xVals[0]);
       double intercept = yVals[0] - slope*xVals[0];
--- src/test/java/il/org/spartan/utils/LinearRegressionTest.java
+++ src/test/java/il/org/spartan/utils/LinearRegressionTest.java
@@ -3,13 +3,30 @@ package il.org.spartan.utils;
 import org.junit.*;
 
 import fluent.ly.*;
+import il.org.spartan.utils.LinearRegression.*;
 
+@SuppressWarnings("static-method")
 public class LinearRegressionTest {
-  @SuppressWarnings("boxing") @Test public void linearRegressionTwoDots() {
+  @Test public void linearRegressionTwoDots() {
     RealNumbersPairList list = new RealNumbersPairList();
     list.record(0.0, 0.0);
     list.record(5.0, 5.0);
     LinearRegression regression = new LinearRegression(list);
-    azzert.assertTrue(regression.getLine().equals(new LinearFunction(1.0, 0.0)));
+    try {
+      azzert.assertTrue(regression.getLine().equals(new LinearFunction(1.0, 0.0)));
+    } catch (@SuppressWarnings("unused") Exception e) {
+      azzert.assertTrue(false);
+    }
+  }
+  
+  @Test public void linearRegressionOneDot() {
+    RealNumbersPairList list = new RealNumbersPairList();
+    list.record(0.0, 0.0);
+    LinearRegression regression = new LinearRegression(list);
+    try {
+      regression.getLine();
+    } catch (@SuppressWarnings("unused") NotEnoughDots e) {
+      azzert.assertTrue(true);
+    }
   }
 }
