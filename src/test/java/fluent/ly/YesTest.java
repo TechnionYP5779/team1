1732162329483f71ac28707e27160bf68e338576
Ofir <ofiralexi@gmail.com>
2018-11-15 15:25:32 +0200

    tests for Yes.java issue #98

---
+++ src/test/java/fluent/ly/YesTest.java
@@ -0,0 +1,25 @@
+package fluent.ly;
+
+import org.junit.*;
+
+// Added suppress warning cause eclipse claims 
+// that the methods can be static although it won't compile
+@SuppressWarnings("static-method")
+public class YesTest {
+  @Test public void forgettingRunnableTest() {
+    assert yes.forgetting(new Runnable() {
+      @Override public void run() { /* redundant*/ }});
+  }
+  @Test public void forgettingObjectTest() {
+    assert yes.forgetting(Integer.toString(5));
+  }
+  @Test public void ignoringBooleanTest() {
+    assert yes.ignoring(false) && yes.ignoring(true);
+  }
+  @Test public void ignoringDoubleTest() {
+    assert yes.ignoring(5.0);
+  }
+  @Test public void ignoringLongTest() {
+    assert yes.ignoring(123);
+  }
+}
