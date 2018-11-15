fcd88b42f5b49cd8a463da55fd7495573a4dc085
yoavz <yoavzuriel@cs.technion.ac.il>
2018-11-15 17:57:18 +0200

    removed null warnings as part of #88

--- src/test/java/il/org/spartan/utils/LongestCommonSubsequenceTest.java
+++ src/test/java/il/org/spartan/utils/LongestCommonSubsequenceTest.java
@@ -4,26 +4,27 @@ import org.junit.*;
 
 import fluent.ly.*;
 
-@SuppressWarnings({ "null", "static-method", "static-access" })  public class LongestCommonSubsequenceTest {
+@SuppressWarnings({ "static-method", "static-access" }) public class LongestCommonSubsequenceTest {
   @Test public void testLongestCommonSubsequenceNotNull() {
-    azzert.falze(new LongestCommonSubsequence("abc", "abc") == null);
+    azzert.assertNotEquals(new LongestCommonSubsequence("abc", "abc"), null);
   }
 
   @Test public void testSimilarityWithEmptyString() {
     azzert.assertEquals(0, (new LongestCommonSubsequence("", "abc")).similarity(), 1E-20);
     azzert.assertEquals(0, (new LongestCommonSubsequence("abc", "")).similarity(), 1E-20);
   }
-  
+
   @Test public void testSimilaritySameString() {
     azzert.assertEquals(1, (new LongestCommonSubsequence("abc", "abc")).similarity(), 1E-20);
   }
-  
+
   @Test public void testSimilarityTotallyDifferent() {
     azzert.assertEquals(0, (new LongestCommonSubsequence("abc", "def")).similarity(), 1E-20);
   }
-  
+
   @Test public void testSimilarityWithSimilarity() {
-    //that number is the ratio between the len of the LCS (4) and the average len of the strings (7)
+    // that number is the ratio between the len of the LCS (4) and the average
+    // len of the strings (7)
     azzert.assertEquals(0.57142857142, (new LongestCommonSubsequence("aHbHbHa", "aKKKbba")).similarity(), 1E-5);
   }
 }
