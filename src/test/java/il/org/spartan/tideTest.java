4a3084c933fc31d6e8ff2aa510c496a3a3b6b1e9
Yarin Shlafir <shlafiry@gmail.com>
2018-11-15 17:02:25 +0200

    tests for tide #109

---
+++ src/test/java/il/org/spartan/tideTest.java
@@ -0,0 +1,24 @@
+package il.org.spartan;
+
+import org.junit.*;
+
+import fluent.ly.*;
+
+@SuppressWarnings("static-method") public class tideTest {
+  @Test @SuppressWarnings("static-access") public void clean() {
+    azzert.assertEquals(tide.clean("  Hello  "), "Hello");
+  }
+  
+   @Test public void eq(){
+    String s1 = "Hello";
+    String s2 = "Hello";
+    assert tide.eq(s1, s2);
+    s1 = null;
+    assert !tide.eq(s1,s2);
+    s1 = "Boy";
+    assert !tide.eq(s1, s2);
+    s2 = null;
+    assert !tide.eq(s1, s2);
+  }
+
+}
