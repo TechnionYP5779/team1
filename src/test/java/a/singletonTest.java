3829fd43b6c475809550eed0d2ede5f0e3767834
shai <shai.zeevi@campus.technion.ac.il>
2018-11-15 12:36:46 +0200

    fixed #93

--- src/test/java/a/singletonTest.java
+++ src/test/java/a/singletonTest.java
@@ -6,14 +6,14 @@ import org.junit.*;
 
 import fluent.ly.*;
 
-@SuppressWarnings("static-method") public class singletonTest {
+@SuppressWarnings({"static-method", "static-access"}) public class singletonTest {
   @Test public void testToList() {
-    Assert.assertEquals(Arrays.asList("hello"), singleton.list("hello"));
+    azzert.assertEquals(Arrays.asList("hello"), singleton.list("hello"));
   }
 
   @Test public void testToArray() {
     final Object[] a = singleton.array("hello");
-    Assert.assertEquals("hello", a[0]);
+    azzert.assertEquals("hello", a[0]);
     azzert.assertEquals(1, a.length);
   }
 }
