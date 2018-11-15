ae8b08f13fed853af9f1377e684e0adea61f8b45
Matan <matan.pugach@campus.technion.ac.il>
2018-11-15 19:31:09 +0200

    Updated the test according to remaks theTest.java #99

--- src/test/java/il/org/spartan/UtilsTest.java
+++ src/test/java/il/org/spartan/UtilsTest.java
@@ -123,7 +123,7 @@ import il.org.spartan.etc.*;
       azzert.assertEquals(Utils.addAll(y,yy),y);
       azzert.assertEquals(Utils.addAll(y,(Iterable<Integer>)x),y);
       azzert.assertFalse(Utils.hasNull(yy));
-      azzert.assertTrue(Utils.hasNull(1,null));
+      azzert.assertTrue(Utils.hasNull(Integer.valueOf(1),null));
       azzert.assertNotNull(Utils.append(new @NotNull Integer[0], Integer.valueOf(4))[0]);
 //      azzert.assertEquals(,);
       azzert.assertNotNull(Utils.delete(new @NotNull Integer[4],3));
