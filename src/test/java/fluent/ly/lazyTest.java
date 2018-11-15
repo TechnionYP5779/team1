ba4158ff4c8582e0ed2787eeee58ec6778b91d7f
shai <shai.zeevi@campus.technion.ac.il>
2018-11-15 16:15:35 +0200

    finished lazyTest #107

--- src/test/java/fluent/ly/lazyTest.java
+++ src/test/java/fluent/ly/lazyTest.java
@@ -1,11 +1,24 @@
 package fluent.ly;
 
-import static org.junit.Assert.*;
+import java.util.function.*;
 
+import org.jetbrains.annotations.*;
 import org.junit.*;
 
-public class lazyTest {
+@SuppressWarnings({ "static-access", "static-method" }) public class lazyTest {
   @Test public void testGet() {
-    fail("Not yet implemented");
+    lazy<@Nullable String> lzy = lazy.get(new Supplier<@Nullable String>() {
+      int cnt;
+
+      @Override public String get() {
+        cnt++;
+        return "foo" + cnt;
+      }
+    });
+    
+    //make sure the computation only happens once
+    azzert.assertEquals("foo1", lzy.get());
+    azzert.assertEquals("foo1", lzy.get());
+    azzert.assertEquals("foo1", lzy.get());
   }
 }
