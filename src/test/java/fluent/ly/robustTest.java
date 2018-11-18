0477134758afdbf36a5683a4195a796c6aff2c0e
saifun <saifunny@gmail.com>
2018-11-18 15:18:16 +0200

    init tests for Row and robust #102 #104

---
+++ src/test/java/fluent/ly/robustTest.java
@@ -0,0 +1,13 @@
+package fluent.ly;
+
+import static il.org.spartan.Utils.*;
+
+import java.util.*;
+
+import org.junit.*;
+
+@SuppressWarnings("static-method") public class robustTest {
+  @Test public void init() {
+    assert true;
+  }
+}
---
+++ src/test/java/il/org/spartan/tables/RowTest.java
@@ -0,0 +1,9 @@
+package il.org.spartan.tables;
+
+import org.junit.*;
+
+@SuppressWarnings("static-method") public class RowTest {
+  @Test public void init() {
+    assert true;
+  }
+}
