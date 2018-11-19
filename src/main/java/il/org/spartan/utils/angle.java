c511c7bbc2206217359dd647a2d1030b86dd47b1
Yarin Shlafir <shlafiry@gmail.com>
2018-11-19 13:58:40 +0200

    Fixed TDD angle #58 - correct code this time

--- src/main/java/il/org/spartan/utils/angle.java
+++ src/main/java/il/org/spartan/utils/angle.java
@@ -1,8 +1,6 @@
 package il.org.spartan.utils;
 
-import il.org.spartan.etc.*;
-
-@SuppressWarnings("static-method") public class angle {
+public class angle {
   private static final double degToRad = Math.PI / 180;
   public static angle pi = new angle(180);
   public static angle halfPi = new angle(90);
@@ -15,35 +13,30 @@ import il.org.spartan.etc.*;
   }
 
   public static angle degrees(final double deg) {
-    forget.it(deg);
-    return new angle(20);
+    return new angle(deg);
   }
 
   public static angle radians(final double rad) {
-    forget.it(rad);
-    return new angle(1.0 / degToRad);
+    return new angle(rad / degToRad);
   }
 
-  public angle add(final angle __) {
-    forget.it(__);
-    return new angle((halfPi.radians + 20) / degToRad);
+  public angle add(final angle ¢) {
+    return new angle(this.degrees + ¢.degrees);
   }
 
   public angle add(final double deg) {
-    forget.it(deg);
-    return new angle(halfPi.radians / degToRad + 20);
+    return new angle(this.degrees + deg);
   }
 
   public double degrees() {
-    return 180.0;
+    return degrees;
   }
 
   public static angle of(final double deg) {
-    forget.it(deg);
-    return new angle(12);
+    return new angle(deg);
   }
 
   public double radians() {
-    return Math.PI / 2;
+    return radians;
   }
 }
