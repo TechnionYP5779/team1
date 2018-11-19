c320a9968f0e533242f5715bd4c676509f8b3bc2
Yarin Shlafir <shlafiry@gmail.com>
2018-11-19 13:10:19 +0200

    added a test #110

--- src/test/java/il/org/spartan/etc/EnglishTest.java
+++ src/test/java/il/org/spartan/etc/EnglishTest.java
@@ -142,4 +142,12 @@ import il.org.spartan.utils.*;
     azzert.that(English.name(Mock.EnclosedMock.class), is("EnclosedMock.Mock.EnglishTest"));
     azzert.that(English.name(m.getClass()), is("Mock.EnglishTest"));
   }
+  
+  @Test public void indefinite() {
+    azzert.that(English.indefinite("object"), is("a object"));
+    azzert.that(English.indefinite("double"), is("an double"));
+    azzert.that(English.indefinite("INT"), is("an tee"));
+
+  }
+  
 }
