e198fd6979d289a203a9273c40f68155505a6f53
saifun <saifunny@gmail.com>
2018-11-20 17:12:31 +0200

    First tests for robust, issues #102 #96

--- src/test/java/fluent/ly/robustTest.java
+++ src/test/java/fluent/ly/robustTest.java
@@ -1,13 +1,38 @@
 package fluent.ly;
-
-import static il.org.spartan.Utils.*;
-
-import java.util.*;
-
+import static fluent.ly.azzert.is;
+import static fluent.ly.box.box;
 import org.junit.*;
 
 @SuppressWarnings("static-method") public class robustTest {
-  @Test public void init() {
-    assert true;
+  @Test public void robustlySupplierNoException() {
+    azzert.that(robust.<Integer>ly(()->(box(2)),(Exception e) -> (box(3))), is(2));
+  }
+  
+  
+  public static Integer throwException(){
+    throw new NullPointerException();
+  }
+  
+  public static Boolean throwBoolException(){
+    throw new NullPointerException();
+  }
+  
+  @Test public void robustlySupplierException() {
+    azzert.that(robust.<Integer>ly(robustTest::throwException, (Exception e) -> (box(3))), is(3));
+  }
+  
+  @Test public void robustlyFalseNoException() {
+    assert robust.lyFalse(() -> true, λ -> {
+      /*it's a Consumer, so it returns nothing*/
+    });
+  }
+  
+  @Test public void robustlyFalseException() {
+    System.out.println(robust.lyFalse(robustTest::throwBoolException, λ -> {
+      /*it's a Consumer, so it returns nothing*/
+    }));
+    assert !robust.lyFalse(robustTest::throwBoolException, λ -> {
+      /*it's a Consumer, so it returns nothing*/
+    });
   }
 }
