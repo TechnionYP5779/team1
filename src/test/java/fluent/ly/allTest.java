38d7f5bacb6af0451f2064deb80431f7075f5d9a
saifun <saifunny@gmail.com>
2018-11-20 21:57:36 +0200

    Tests for all :) 100% coverage #103

--- src/test/java/fluent/ly/allTest.java
+++ src/test/java/fluent/ly/allTest.java
@@ -1,13 +1,54 @@
 package fluent.ly;
 
-//import static fluent.ly.azzert.*;
-//import static fluent.ly.box.*;
+import java.util.*;
+
+import static fluent.ly.box.*;
 
 import org.junit.*;
 
+import org.jetbrains.annotations.*;
+
 @SuppressWarnings("static-method") public class allTest {
-  @Test public void allNotNullIterable() {
-    assert true;
+  @Test public void allNotNullIterableNullCollection() {
+    assert !all.notNull((ArrayList<Integer>)null);
+  }
+  
+  @Test public void allNotNullIterableNullValue() {
+    ArrayList<Integer> a = new ArrayList<>();
+    a.add(box(3));
+    a.add(null);
+    a.add(box(5));
+    
+    assert !all.notNull(a);
+  }
+  
+  @Test public void allNotNullIterableNoNullValue() {
+    ArrayList<Integer> a = new ArrayList<>();
+    a.add(box(3));
+    a.add(box(7));
+    a.add(box(5));
+    
+    assert all.notNull(a);
+  }
+  
+  @Test public void allNotNullArrayNullCollection() {
+    assert !all.notNull((@Nullable Integer[]) null);
+  }
+  
+  @Test public void allNotNullArrayNullValue() {
+    @Nullable Integer a[] = new @Nullable Integer[3];
+    a[0] = (box(3));
+    a[1] = null;
+    a[2] = (box(5));
+    assert !all.notNull(a);
+  }
+  
+  @Test public void allNotNullArrayNoNullValue() {
+    @Nullable Integer a[] = new @Nullable Integer[3];
+    a[0] = (box(3));
+    a[1] = (box(7));
+    a[2] = (box(5));
+    assert all.notNull(a);
   }
 }
 
